f541c7b3deb14d1d1dd36fe38f5253f1675f86ce
zsin520 <zachsin99@gmail.com>
2026-02-13 11:00:55 -0800

    Successful setup and initial test run

--- test_coverage_lab/ci_lab/src/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from src import counter  # Ensure you import the routes
-from src.counter import app  # Expose Flask app for testing
--- test_coverage_lab/tdd_lab/src/__init__.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-# we need to import the unit under test - counter
-from src.counter import app 
-
-# we need to import the file that contains the status codes
-from src import status 
