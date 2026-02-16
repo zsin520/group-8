5b2e7384a18f4357dfda844b592b62fd24bfe6da
zsin520 <zachsin99@gmail.com>
2026-02-16 13:32:05 -0800

    Introduce small, controlled failure in get_counter

--- ci_lab/src/counter.py
+++ ci_lab/src/counter.py
@@ -43,7 +43,7 @@ def get_counter(name):
     """Retrieve an existing counter"""
     if name not in COUNTERS:
         return jsonify({"error": f"Counter '{name}' not found"}), HTTPStatus.NOT_FOUND
-    return jsonify({name: COUNTERS[name]}), HTTPStatus.OK
+    return jsonify({name: COUNTERS[name]}), HTTPStatus.CREATED
 
 
 @app.route("/counters/<name>", methods=["PUT"])
